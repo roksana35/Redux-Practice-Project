f6a0d6ab461a4b1d3625e0d9118bd5e560d135c2
roksana35 <roksana7235@gmail.com>
2024-11-24 17:22:15 +0600

    featch data and use this data in react app with redux

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllTodos } from '../services/actions/todosActions';
 
 const Todo = () => {
+    const {isLoading,todos,error} = useSelector((state) => state);
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(getAllTodos())
+    },[])
     return (
         <div>
             <h2>Todos App</h2>
+            {isLoading && <h3>Loading...</h3>}
+            {error && <h3>{error.message}</h3>}
+            <section>
+                {todos && todos.map((todo) =>{
+                    return <article key={todo.id}>
+                        <h2>{todo.id}</h2>
+                        <h2>{todo.title}</h2>
+                        
+                    </article>
+                })}
+            </section>
         </div>
     );
 };
